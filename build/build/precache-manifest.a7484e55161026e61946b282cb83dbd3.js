de54351b4829e9a89c86dfa3d58373a08efe75ff
劉沐昆 <liumukun@liumukuns-MacBook-Pro.local>
2020-10-04 09:32:56 +0100

    change mobile nav view

---
+++ build/build/precache-manifest.a7484e55161026e61946b282cb83dbd3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4e278fc598e37b255636cc0f7a7288d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6401a2028db1cad01cb1",
+    "url": "/static/css/main.60a2df9c.chunk.css"
+  },
+  {
+    "revision": "232bf7fe0cbee02b5065",
+    "url": "/static/js/2.49bd0038.chunk.js"
+  },
+  {
+    "revision": "ab4368d17c9657deaf626b9459f8b13a",
+    "url": "/static/js/2.49bd0038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6401a2028db1cad01cb1",
+    "url": "/static/js/main.32019dff.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "0846095e7a67f52d96d2ffbf50f815d1",
+    "url": "/static/media/icon.0846095e.png"
+  }
+]);
