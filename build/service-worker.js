8c7b3ab67da6025e9d0cd0154174d663ab0677a8
劉沐昆 <liumukun@liumukuns-MacBook-Pro.local>
2020-10-04 09:35:35 +0100

    change mobile nav view

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.38423fe240c6a075721a151c6675837a.js"
+  "/precache-manifest.a7484e55161026e61946b282cb83dbd3.js"
 );
 
 self.addEventListener('message', (event) => {
